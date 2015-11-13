19518c440fd6b114231e0699257f5a2b60519811
Danil Gorbenko <busymangorbenko@gmail.ru>
2015-11-13 09:49:07 +0200

    added compact_arguments method to utils

--- lib/vkontakte_api/utils.rb
+++ lib/vkontakte_api/utils.rb
@@ -23,6 +23,26 @@ module VkontakteApi
           argument
         end
       end
+
+      # Returns a hash without empty fields. Field is empty, when it's value is one of these:
+      # '' - empty string
+      # '  ' - string consisting only of whitespaces
+      # [] - array without elements
+      # {} - hash without fields
+      # @param [Hash] arguments The arguments to compact.
+      # @return [Hash] Compacted arguments.
+      def compact_arguments(arguments)
+        arguments.inject({}) do |compacted_args, (arg_name, arg_value)|
+          unless arg_value.respond_to?(:empty?)
+            # if argument is not array, hash or string, convert it to string and strip it
+            arg_value = arg_value.to_s.strip
+          end
+
+          # miss empty fields
+          compacted_args[arg_name] = arg_value unless arg_value.empty?
+          compacted_args
+        end
+      end
     end
   end
 end
